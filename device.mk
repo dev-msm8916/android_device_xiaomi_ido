a5f5d79dd3860d55c1b73c7a38f4f16997e59d60
karthik558 <karthik.lal558@gmail.com>
2021-05-02 16:23:35 +0000

    ido: Force triple frame buffers

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Force triple frame buffers
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8916
