c53c21efdf2fa07868fd920b37a5dcec6b592553
xoo2001 <ekora2001@gmail.com>
2021-06-19 21:27:41 +0700

    ido: tune dexopt

--- device.mk
+++ device.mk
@@ -205,9 +205,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dexopt
 PRODUCT_PROPERTY_OVERRIDES += \
-   pm.dexopt.install=quicken \
+   pm.dexopt.install=speed-profile \
    pm.dexopt.first-boot=quicken \
-   pm.dexopt.boot=quicken
+   pm.dexopt.boot=extract
 
 # Privapp-permissions whitelisting
 PRODUCT_PROPERTY_OVERRIDES += \
