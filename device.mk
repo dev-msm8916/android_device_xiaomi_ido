220549741adbbd945f8efaaeb5bef32aad7439d2
xoo2001 <ekora2001@gmail.com>
2021-06-19 20:43:30 +0700

    ido: enable memory optimizations

--- device.mk
+++ device.mk
@@ -190,6 +190,19 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2017-01-01
 
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.vendor.qti.sys.fw.bservice_enable=true \
+   ro.vendor.qti.sys.fw.bservice_limit=5 \
+   ro.vendor.qti.sys.fw.bservice_age=5000 \
+   ro.vendor.qti.sys.fw.use_trim_settings=true \
+   ro.vendor.qti.sys.fw.empty_app_percent=50 \
+   ro.vendor.qti.sys.fw.trim_empty_percent=100 \
+   ro.vendor.qti.sys.fw.trim_cache_percent=100 \
+   ro.vendor.qti.sys.fw.trim_enable_memory=2147483648 \
+   ro.vendor.qti.sys.fw.bg_apps_limit=7 \
+   ro.config.max_starting_bg=2
+
 # Dexopt
 PRODUCT_PROPERTY_OVERRIDES += \
    pm.dexopt.install=quicken \
